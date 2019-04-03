29fc51dd047f4036c6ab46af1cc278047a6e8c2b
Sarah Gibson <sgibson@turing.ac.uk>
2019-04-03 13:37:29 +0100

    Update inside-frame.js to scrape a url of CFF file
    Update inside-frame.js to scrape a url page and load the contents
    as a YAML object

--- extension/inside-frame.js
+++ extension/inside-frame.js
@@ -2,26 +2,42 @@ if (typeof chrome !== "undefined"){
     browser = chrome
 }
 
-var parts = window.name.split("#")
-var color = parts[0]
-var url = decodeURI(parts[1])
+yaml = require('js-yaml')
 
-$(".button")
-    .fadeIn()
-    .click(function(){
-        // todo: replace this scary alert with a message up to the
-        // unpaywall.js content script, which can then pop up
-        // a prettier and mre useful dialog box.
-        alert("CITATION")
-        if (color == "black") {
-            alert("The Unpaywall extension " +
-            "couldn't find any legal open-access version of this article.");
-        }
-        else {
-            // send a message to the extension, so it can open this url
-            // in a new tab
-            /*browser.runtime.sendMessage({"upUrl": url});*/
-        }
-    })
+const rp = require('request-promise');
+const url = //placeholder for url of raw CITATION.cff file
 
-$(".button").addClass(color)
+var file = rp(url)
+  .then(function(html) {
+    // console.log(html);
+    file = yaml.load(html)
+  })
+  .catch(function(err) {
+    //handle error
+  });
+
+
+
+// var parts = window.name.split("#")
+// var color = parts[0]
+// var url = decodeURI(parts[1])
+//
+// $(".button")
+//     .fadeIn()
+//     .click(function(){
+//         // todo: replace this scary alert with a message up to the
+//         // unpaywall.js content script, which can then pop up
+//         // a prettier and mre useful dialog box.
+//         alert("CITATION")
+//         if (color == "black") {
+//             alert("The Unpaywall extension " +
+//             "couldn't find any legal open-access version of this article.");
+//         }
+//         else {
+//             // send a message to the extension, so it can open this url
+//             // in a new tab
+//             /*browser.runtime.sendMessage({"upUrl": url});*/
+//         }
+//     })
+//
+// $(".button").addClass(color)
