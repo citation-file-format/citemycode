ea34a5924d6fcc9c7d7554275a6887832752df34
sgibson91 <44771837+sgibson91@users.noreply.github.com>
2019-04-03 14:40:17 +0100

    Revert "Update inside-frame.js to scrape webpage and load as YAML"

--- extension/js/request-promise-master/test/fixtures/require/afterwards.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var rp = require('../../../lib/rp.js');
-var request = require('request');
-
-var rpHasThen = rp('http://localhost:4000/200').then !== undefined;
-var requestHasNoThen = request('http://localhost:4000/200').then === undefined;
-
-console.log('rp: ' + rpHasThen + ', request: ' + requestHasNoThen);
